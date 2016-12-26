20388b508acbfd9c5524352fff9d624c62e89d50
temp-impl <temp-impl@users.noreply.github.com>
2016-12-26 15:12:23 +0900

    add samples

--- web-api/src/main/kotlin/my/webapi/controllers/BookController.kt
+++ web-api/src/main/kotlin/my/webapi/controllers/BookController.kt
@@ -21,6 +21,9 @@ open class BookController(
     @GetMapping("/{id:\\d+}")
     open fun get(@PathVariable id: Long): Book? = repository.findById(id)
 
+    @GetMapping("/{id:\\d+}/amount")
+    open fun addAmount(@PathVariable id: Long, @RequestParam(required = false) value: Int?): Int = repository.addAmount(id, value ?: 1)
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     open fun post(@RequestBody book: Book): Book = repository.create(book)
@@ -32,4 +35,7 @@ open class BookController(
     @DeleteMapping("/{id:\\d+}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     open fun delete(@PathVariable id: Long){ repository.deleteById(id) }
+
+    @GetMapping("/version")
+    open fun version(): String = repository.version()
 }
--- web-api/src/main/kotlin/my/webapi/repository/BookRepository.kt
+++ web-api/src/main/kotlin/my/webapi/repository/BookRepository.kt
@@ -4,26 +4,65 @@ import my.common.db.BookTable
 import my.common.domain.Book
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.statements.UpdateBuilder
+import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 
 @Repository
 @Transactional
 open class BookRepository {
+    /**
+     * SELECT * FROM BookTable
+     */
     open fun findAll(): Iterable<Book> = BookTable.selectAll().map { it.fromRow() }
 
+    /**
+     * SELECT * FROM BookTable WHERE id = :id
+     */
     open fun findById(id: Long): Book? = BookTable.select { BookTable.id eq id }.singleOrNull()?.fromRow()
 
+    /**
+     * INSERT INTO BookTable VALUES(:source)
+     */
     open fun create(source: Book): Book = source.apply {
         id = BookTable.insert(source.toRow())[BookTable.id]
     }
 
+    /**
+     * UPDATE BookTable
+     * SET name = :source.name, amount = :source.amount
+     * WHERE id = :source.id
+     */
     open fun update(source: Book): Int = BookTable.update({ BookTable.id eq source.id!! }) {
         it[name] = source.name
         it[amount] = source.amount
     }
 
+    /**
+     * UPDATE BookTable
+     * SET amount = amount + :value
+     * WHERE id = :id
+     */
+    open fun addAmount(id: Long, value: Int): Int = BookTable.update({ BookTable.id eq id }) {
+        with(SqlExpressionBuilder) {
+            it.update(BookTable.amount, BookTable.amount + value)
+        }
+    }
+
+    /**
+     * DELETE FROM BookTable WHERE id = :id
+     */
     open fun deleteById(id: Long): Int = BookTable.deleteWhere { BookTable.id eq id }
+
+    /**
+     * Raw query sample
+     */
+    open fun version(): String {
+        val conn = TransactionManager.current().connection
+        val statement = conn.createStatement()
+        statement.execute("SELECT VERSION();")
+        return statement.resultSet.let { it.next(); it.getString(1) }
+    }
 
     private fun Book.toRow(): BookTable.(UpdateBuilder<*>) -> Unit = {
         it[name] = this@toRow.name
